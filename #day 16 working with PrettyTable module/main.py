dc4552ce8c3943b13d36ddc2375387f62c0b8f8c
PushpaKumar <112679167+Pushpakumar02@users.noreply.github.com>
2024-03-18 18:34:43 +0530

    updated the file

---
+++ #day 16 working with PrettyTable module/main.py
@@ -0,0 +1,25 @@
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikachu","Squiritle","Charmander"])
+table.add_column("Type",["Electric","Water","Fire"])
+table.align = "c"
+print(table)
+
+# from turtle import Turtle, Screen
+#
+# # import turtle
+# # tommy = turtle.Turtle()
+# tommy = Turtle()  # this is the first object
+# print(tommy)
+# tommy.shape("turtle")
+# tommy.forward(100)
+# tommy.left(120)
+# tommy.forward(100)
+# tommy.left(120)
+# tommy.forward(100)
+# tommy.color("blue")
+# my_screen = Screen()  # this is the second object
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
