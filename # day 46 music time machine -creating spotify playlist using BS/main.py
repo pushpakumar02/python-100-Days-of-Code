065f0ab33a942a7e5176306f4fbe4b593b2224d4
PushpaKumar <112679167+Pushpakumar02@users.noreply.github.com>
2024-03-27 11:45:08 +0530

    updated the files

---
+++ # day 46 music time machine -creating spotify playlist using BS/main.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+CLIENT_ID = "1d78f9c79b8e49ffa0d8748b13e180ca"
+CLIENT_SCERET = "0e6a24715ebb4bc9bd02569ba83bc6fa"
+
+URl = "https://www.billboard.com/charts/hot-100/"
+
+# Scraping Billboard 100
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+response = requests.get(URl + date)
+soup = BeautifulSoup(response.text, "html.parser")
+song_name_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_name_spans]
+# print(song_names)
+
+#Spotify Authentication
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope= "playlist-modify-private",
+        redirect_uri= "http://example.com",
+        client_id= CLIENT_ID,
+        client_secret= CLIENT_SCERET,
+        show_dialog=True,
+        cache_path="token.txt",
+        username="pushpakumar"
+    ),
+requests_timeout=30
+)
+
+user_id = sp.current_user()["id"]
+
+# Searching Spotify for songs by title
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}",type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in the spotify. Skipped.")
+# Creating a new private playlist in Spotify
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+print(playlist)
+
+# Adding songs found into the new playlist
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
