333f09bee5ad9c849d2e5e79662f27b5284110b2
PushpaKumar <112679167+Pushpakumar02@users.noreply.github.com>
2024-03-29 09:19:12 +0530

    Update main.py

--- # day 47 Amazon price checker/main.py
+++ # day 47 Amazon price checker/main.py
@@ -2,7 +2,7 @@ import requests
 import smtplib
 
 MY_EMAIL = "pushpakumarpushpakumar70@gmail.com"
-PASSWORD = "thpx ynll qwen mmjx"
+PASSWORD = "thpx ynll qwen "
 
 from bs4 import BeautifulSoup
 URL = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
