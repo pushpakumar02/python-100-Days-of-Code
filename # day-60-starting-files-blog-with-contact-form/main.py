a33d25d4ca9559acd85121cbe01dc0d3cd3678a0
PushpaKumar <112679167+Pushpakumar02@users.noreply.github.com>
2024-04-20 09:55:29 +0530

    Update main.py

--- # day-60-starting-files-blog-with-contact-form/main.py
+++ # day-60-starting-files-blog-with-contact-form/main.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request
 import requests
 import smtplib
 
-MY_EMAIL = "pushpakumarpushpakumar70@gmail.com"
-PASSWORD = "thpx ynll qwen mmjx"
+MY_EMAIL = ".........."
+PASSWORD = "......."
 
 # USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
