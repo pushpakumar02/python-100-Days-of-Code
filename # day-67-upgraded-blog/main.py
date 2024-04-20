ccea758c5b9180e84554924754561202b4400450
PushpaKumar <112679167+Pushpakumar02@users.noreply.github.com>
2024-04-20 09:53:15 +0530

    Update main.py

--- # day-67-upgraded-blog/main.py
+++ # day-67-upgraded-blog/main.py
@@ -10,7 +10,7 @@ from flask_ckeditor import CKEditor, CKEditorField
 from datetime import date
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = '.......'
 Bootstrap5(app)
 ckeditor = CKEditor(app)
 
