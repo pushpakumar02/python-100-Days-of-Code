344c6685ece775bad8c5055a7c60682aef3d20ac
PushpaKumar <112679167+Pushpakumar02@users.noreply.github.com>
2024-04-20 09:51:44 +0530

    Update main.py

--- # day-69-blog-with-users/main.py
+++ # day-69-blog-with-users/main.py
@@ -15,7 +15,7 @@ import os
 # import smtplib
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = '........'
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
