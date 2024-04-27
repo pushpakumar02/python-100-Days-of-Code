78554c638ad6211401ddd647359015e761d4c9b3
PushpaKumar <112679167+Pushpakumar02@users.noreply.github.com>
2024-04-27 08:35:21 +0530

    update the file

---
+++ # day 82 text_to_morse_converter/main.py
@@ -0,0 +1,25 @@
+morse_code_dict = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+                   'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+                   'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+                   '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', ' ': ' '}
+
+
+def text_to_morse_code(text):
+    morse_code = ""
+    for char in text.upper():
+        if char in morse_code_dict:
+            morse_code += morse_code_dict[char] + " "
+        else:
+            morse_code += " "
+    return morse_code
+
+
+def main():
+    input_text = input("Enter the Input Text: ")
+    morse_code = text_to_morse_code(input_text)
+    print(f"Morse_code: {morse_code}")
+
+
+if __name__ == "__main__":
+    main()
